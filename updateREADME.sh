9fa7ab3c24d5582f68ecab11de5317bbac4e6105
Barad Ghimire <baradghimire@gmail.com>
2020-07-22 23:19:55 +1000

    Update script to replace line #1 of README.md with img tag

--- updateREADME.sh
+++ updateREADME.sh
@@ -2,5 +2,6 @@
 UNSPLASH_TOKEN=$1
 UNSPLASH_RESPONSE=`curl -s -X GET https://api.unsplash.com/photos/random/?query=nature\&featured=true -H "Authorization: Client-ID $UNSPLASH_TOKEN"`
 RAW_URL=`echo "$UNSPLASH_RESPONSE" | jq -r '.urls.raw'`
-IMG_TAG="<img src=\""$RAW_URL"/&fm=jpg&crop=faces&fit=crop&h=540&w=1920\"/>"
-echo $IMG_TAG
+RAW_TAG="<img src=\""$RAW_URL"/&fm=jpg&crop=faces&fit=crop&h=540&w=1920\"/>"
+IMG_TAG=$(sed 's|&|\\&|g' <<< $RAW_TAG)
+sed -i "" "1s~.*~$IMG_TAG~" README.md
