3e2a143d25ccadaf4df00bb5ae04475d5c907f96
Barad Ghimire <baradghimire@gmail.com>
2020-07-22 23:41:23 +1000

    Fix quotes again

--- updateREADME.sh
+++ updateREADME.sh
@@ -4,4 +4,4 @@ UNSPLASH_RESPONSE=`curl -s -X GET https://api.unsplash.com/photos/random/?query=
 RAW_URL=`echo "$UNSPLASH_RESPONSE" | jq -r '.urls.raw'`
 RAW_TAG='<img src=\"'$RAW_URL'/&fm=jpg&crop=faces&fit=crop&h=540&w=1920\"/>'
 IMG_TAG=$(sed 's|&|\\&|g' <<< $RAW_TAG)
-sed -i "" "1s~.*~$IMG_TAG~" README.md
+sed -i "1s~.*~$IMG_TAG~" README.md
