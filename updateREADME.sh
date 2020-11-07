744ba1226e59281494e8823a12ece8de68b62139
Barad Ghimire <baradghimire@gmail.com>
2020-11-07 18:22:22 +1100

    Cleanup and updates

--- updateREADME.sh
+++ updateREADME.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-UNSPLASH_TOKEN=$1
-UNSPLASH_RESPONSE=`curl -s -X GET https://api.unsplash.com/photos/random/?query=nature -H "Authorization: Client-ID $UNSPLASH_TOKEN"`
-RAW_URL=`echo "$UNSPLASH_RESPONSE" | jq -r '.urls.raw'`
-USR_NAME=`echo "$UNSPLASH_RESPONSE" | jq -r '.user.username'`
-IMG_LINK=`echo "$UNSPLASH_RESPONSE" | jq -r '.links.html'`
-RAW_TAG='<img src=\"'$RAW_URL'/&fm=jpg&crop=faces&fit=crop&h=540&w=1920\"/><sup>Photo by ['$USR_NAME']('$IMG_LINK') on [Unsplash](https://unsplash.com)</sup>'
-HEADER=$(sed 's|&|\\&|g' <<< $RAW_TAG)
-sed -i "1s~.*~$HEADER~" README.md
+TKN=$1
+RES=`curl -s -X GET https://api.unsplash.com/photos/random/?query=nature -H "Authorization: Client-ID $TKN"`
+URL=`echo "$RES" | jq -r '.urls.raw'`
+USR=`echo "$RES" | jq -r '.user.username'`
+LNK=`echo "$RES" | jq -r '.links.html'`
+TDY=`date +'%d/%m/%Y'`
+TAG='<img src=\"'$URL'/&fm=jpg&crop=faces&fit=crop&h=540&w=1920\"/><sup>Photo by ['$USR']('$LNK') on [Unsplash](https://unsplash.com). Last updated: ['$TDY'](https://github.com/baradghimire/baradghimire/blob/main/.github/workflows/autoUpdateREADME.yml)</sup>'
+HDR=$(sed 's|&|\\&|g' <<< $TAG)
+sed -i "1s~.*~$HDR~" README.md
